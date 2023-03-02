e9bbc45a147f5c200e234d264dc148af9110d7e3
Violeta <holasoyviole@gmail.com>
2023-03-02 20:12:23 -0300

    form aún no funciona por completo

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,4 +1,73 @@
-import React from "react";
-import './Form.css';
+import './Form.css'
+import React from "react"
 import { validation } from '../validation';
-import '../../App.js'
+import { useState } from 'react'
+
+const Form = (props) => {
+    const [userData, setUserData] = useState({
+        username:'',
+        password:'',
+    });
+
+    const [errors, setErrors] = useState({
+        username:'',
+        password:'',
+    });
+
+    const handleInputChange = (e) => {
+        setErrors(
+            validation({
+                ...userData,
+                [e.target.name]: e.target.value,
+            })
+        )
+        setUserData({
+            ...userData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.login(userData)
+
+        const propsArr = Object.keys(errors)
+            if (propsArr.length === 0) {
+                setErrors({
+                    username: '',
+                    password: ''
+            })
+
+                setUserData({
+                    username: '',
+                    password: ''
+            })
+        } else alert('Debe rellenar todos los campos')
+    }
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+            <label htmlFor='username'> Username: </label>
+            <input 
+                type='text'
+                name='username'
+                value={userData.username}
+                oneChange={handleInputChange}
+                />
+            {errors.username !== ''? <p className='danger' >{errors.username}</p> : ''}
+            <label htmlFor='password'> Password: </label>
+            <input 
+                type='text'
+                name='password'
+                value={userData.password}
+                oneChange={handleInputChange}
+                />
+            {errors.password !== ''? <p className='danger' >{errors.password}</p> : ''}
+            <button type='submit'> Login </button>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
