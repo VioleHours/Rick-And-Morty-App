eb915ae7c65a4a6775f3aa029eb7560b8a60d7b3
Violeta <holasoyviole@gmail.com>
2023-03-03 00:06:23 -0300

    Nav bien acomodado

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,17 +1,26 @@
 import './NavBar.css';
 import SearchBar from '../SearchBar/SearchBar.jsx';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-export default function NavBar(props){
-    return (
-        <div>
-            <Link to={`/home`}>
-                <span>Home</span>
-            </Link>
-            <Link to={`/about`}>
-                <span>About</span>
-            </Link>
-            <SearchBar onSearch={props.onSearch}/>
-        </div>
-    )
+const NavBar = ({onSearch}) => {
+    const url = useLocation()
+    if (url.pathname !== '/'){
+        return (
+            <nav>
+                <SearchBar onSearch={onSearch}/>
+                <Link to={`/about`}>
+                    <span>About</span>
+                </Link>
+                <Link to={`/home`}>
+                    <span>Home</span>
+                </Link>
+                <Link to={`/favorites`}>
+                    <span>Favorite</span>
+                </Link>
+            </nav>
+        )     
+    }
+    return 
 }
+
+export default NavBar;
